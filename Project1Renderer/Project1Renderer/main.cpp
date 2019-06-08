023f68cf68a41bc4a58bcdef14e5693b4251667b
Spirit-78 <48299218+Spirit-78@users.noreply.github.com>
2019-06-08 18:56:34 +0200

    simple triangles

--- Project1Renderer/Project1Renderer/main.cpp
+++ Project1Renderer/Project1Renderer/main.cpp
@@ -39,6 +39,14 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 	}
 }
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+{
+	line(t0, t1, image, color);
+	line(t1, t2, image, color);
+	lime(t2, t0, image, color);
+}
+
+
 int main(int argc, char** argv)
 {
 	TGAImage image(100,100,TGAImage::RGB);
