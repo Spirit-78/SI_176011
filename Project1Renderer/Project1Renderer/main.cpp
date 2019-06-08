a5aacd7514525c4a3cfdf36f4ac6d691a604c6fd
Spirit-78 <48299218+Spirit-78@users.noreply.github.com>
2019-06-09 01:54:06 +0200

    final fixes of tut3

--- Project1Renderer/Project1Renderer/main.cpp
+++ Project1Renderer/Project1Renderer/main.cpp
@@ -4,20 +4,30 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <limits>
+#include <cstdlib>
+#include <algorithm>
 const TGAColor white = TGAColor(255,255,255,255);
 const TGAColor red = TGAColor(255,0,0,255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor blue = TGAColor(0, 0, 255, 255);
 const int width = 800;
 const int height = 800;
 Model *model = NULL;
 
-//Vec3f barycentric(Vec2i *pts, Vec2i P)
-//{
-//	Vec3f u = cross(Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]), Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]));
-//	if (std::abs(u[2]) < 1)
-//		return Vec3f(-1, 1, 1);
-//	return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
-//}
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P)
+{
+	Vec3f s[2];
+	for (int i = 2; i--; ) {
+		s[i][0] = C[i] - A[i];
+		s[i][1] = B[i] - A[i];
+		s[i][2] = A[i] - P[i];
+	}
+	Vec3f u = cross(s[0], s[1]);
+	if (std::abs(u[2]) > 1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+		return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+	return Vec3f(-1, 1, 1);
+}
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
@@ -56,42 +66,34 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 	}
 }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color)
+Vec3f world2screen(Vec3f v)
 {
-	if (t0.y == t1.y && t0.y == t2.y)
-		return;
-	if (t0.y > t1.y)
-		std::swap(t0, t1);
-	if (t0.y > t2.y)
-		std::swap(t0, t2);
-	if (t1.y > t2.y)
-		std::swap(t1, t2);
-	int total_height = t2.y - t0.y;
-	for (int i = 0; i <total_height; i++)
-	{
-		bool second_half = i > t1.y - t0.y || t1.y == t0.y;
-		int segment_height = second_half? t2.y - t1.y : t1.y - t0.y;
-		float alpha = (float)i/ total_height;
-		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
-		Vec2i A = t0 + (t2 - t0)*alpha;
-		Vec2i B = second_half ? t1 + (t2 - t1)*beta : t0 + (t1 - t0)*beta;
-		if (A.x > B.x)
-			std::swap(A, B);
-		for (int j = A.x; j <= B.x; j++)
-			image.set(j, t0.y+1, color);
-	}
+	return Vec3f(int((v.x + 1.)*width / 2. + .5), int((v.y + 1.)*height / 2. + .5), v.z);
+}
 
-	for (int y = t1.y; y <= t2.y; y++)
-	{
-		int segment_height = t2.y - t1.y + 1;
-		float alpha = (float)(y - t0.y) / total_height;
-		float beta = (float)(y - t1.y) / segment_height;
-		Vec2i A = t0 + (t2 - t0)*alpha;
-		Vec2i B = t1 + (t2 - t1)*beta;
-		if (A.x > B.x) 
-			std::swap(A, B);
-		for (int j = A.x; j <= B.x; j++)
-			image.set(j, y, color); 
+void triangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color)
+{
+	Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+	Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+	Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 2; j++) {
+			bboxmin[j] = std::max(0.f, std::min(bboxmin[j], pts[i][j]));
+			bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+		}
+	}
+	Vec3f P;
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+			Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
+			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+			P.z = 0;
+			for (int i = 0; i < 3; i++) P.z += pts[i][2] * bc_screen[i];
+			if (zbuffer[int(P.x + P.y*width)] < P.z) {
+				zbuffer[int(P.x + P.y*width)] = P.z;
+				image.set(P.x, P.y, color);
+			}
+		}
 	}
 }
 
@@ -114,42 +116,25 @@ void rasterize(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color, int ybuffer[
 
 int main(int argc, char** argv)
 {
-	if (argc == 2)
+	if (2 == argc) {
 		model = new Model(argv[1]);
-	else
-		model = new Model("obj/african_head.obj");
-
-	TGAImage image(width,height,TGAImage::RGB);
-	image.set(52,42,red);
-	Vec3f light_dir(0, 0, -1);
-
-
-	for (int i = 0; i < model->nfaces(); i++) {
-		std::vector<int> face = model->face(i);
-		Vec2i screen_coords[3];
-		Vec3f world_coords[3];
-		for (int j = 0; j < 3; j++) {
-			Vec3f v = model->vert(face[j]);
-			screen_coords[j] = Vec2i((v.x + 1.)*width / 2., (v.y + 1.)*height / 2.);
-			world_coords[j] = v;
-		}
-		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
-		n.normalize();
-		float intensity = n * light_dir;
-		if (intensity > 0) {
-			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
-		}
+	}
+	else {
+		model = new Model("obj/african_head/african_head.obj");
 	}
 
-	TGAImage render(width, 16, TGAImage::RGB);
-	int ybuffer[width];
-	for (int i = 0; i < width; i++)
-		ybuffer[i] = std::numeric_limits<int> ::min();
-	rasterize(Vec2i(20, 34), Vec2i(744, 400), render, red, ybuffer);
-	rasterize(Vec2i(120, 434), Vec2i(444, 400), render, green, ybuffer);
-	rasterize(Vec2i(330, 463), Vec2i(594, 200), render, blue, ybuffer);
+	float *zbuffer = new float[width*height];
+	for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
 
-	image.flip_vertically();
+	TGAImage image(width, height, TGAImage::RGB);
+	for (int i = 0; i < model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		Vec3f pts[3];
+		for (int i = 0; i < 3; i++) pts[i] = world2screen(model->vert(face[i]));
+		triangle(pts, zbuffer, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+	}
+
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	delete model;
 	return 0;
