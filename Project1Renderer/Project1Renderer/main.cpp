0cae52945493191972cc6a601213f237eeb96a1a
Spirit-78 <48299218+Spirit-78@users.noreply.github.com>
2019-06-09 18:34:06 +0200

    tut9 starting with shaders

--- Project1Renderer/Project1Renderer/main.cpp
+++ Project1Renderer/Project1Renderer/main.cpp
@@ -1,156 +1,121 @@
-#include "tgaimage.h"
-#include "geometry.h"
-#include "model.h"
-#include <vector>
+#include <GL/glew.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include <cmath>
-#include <limits>
-#include <cstdlib>
-#include <algorithm>
-#include "our_gl.h"
 
-const TGAColor white = TGAColor(255,255,255,255);
-const TGAColor red = TGAColor(255,0,0,255);
-const TGAColor green = TGAColor(0, 255, 0, 255);
-const TGAColor blue = TGAColor(0, 0, 255, 255);
-const TGAColor yellow = TGAColor(255, 255, 0, 255);
-const int width = 800;
-const int height = 800;
-float *shadowbuffer = NULL;
-Model *model = NULL;
-int *zbuffer = NULL;
-Vec3f light_dir(1,1,1);
-Vec3f camera(0, 0, 3);
-Vec3f eye(1, 1, 4);
-Vec3f center(0, 0, 0);
-Vec3f up(0, 1, 0);
-const int depth = 255;
+#define USE_SHADERS 1
+GLuint  prog_hdlr;
 
-Vec3f m2v(Matrix m) {
-	return Vec3f(m[0][0] / m[3][0], m[1][0] / m[3][0], m[2][0] / m[3][0]);
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 1024;
+const float camera[] = { .6,0,1 };
+const float light0_position[4] = { 1,1,1,0 };
+
+void render_scene(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluLookAt(camera[0], camera[1], camera[2], 0, 0, 0, 0, 1, 0);
+	glColor3f(.8, 0., 0.);
+	glutSolidTeapot(.7);
+	glutSwapBuffers();
 }
 
-struct GouraudShader : public IShader {
-	Vec3f varying_intensity;
-
-	virtual Vec4f vertex(int iface, int nthvert) {
-		varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert)*light_dir);
-		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); 
-		return Viewport * Projection*ModelView*gl_Vertex;
+void process_keys(unsigned char key, int x, int y) {
+	if (27 == key) {
+		exit(0);
 	}
-
-	virtual bool fragment(Vec3f bar, TGAColor &color) {
-		float intensity = varying_intensity * bar; 
-		if (intensity > .85) intensity = 1;
-		else if (intensity > .60) intensity = .80;
-		else if (intensity > .45) intensity = .60;
-		else if (intensity > .30) intensity = .45;
-		else if (intensity > .15) intensity = .30;
-		else intensity = 0;
-		color = TGAColor(255, 255, 255)*intensity;
-		return false;                      
-	}
-};
-
-struct Shader : public IShader {
-	mat<2, 3, float> varying_uv;
-	mat<3, 3, float> varying_nrm;
-
-	mat<4, 4, float> uniform_M;
-	mat<4, 4, float> uniform_MIT;
-	mat<4, 4, float> uniform_Mshadow;
-
-};
-
-float max_elevation_angle(float *zbuffer, Vec2f p, Vec2f dir) {
-	float maxangle = 0;
-	for (float t = 0.; t < 1000.; t += 1.) {
-		Vec2f cur = p + dir * t;
-		if (cur.x >= width || cur.y >= height || cur.x < 0 || cur.y < 0) return maxangle;
-
-		float distance = (p - cur).norm();
-		if (distance < 1.f) continue;
-		float elevation = zbuffer[int(cur.x) + int(cur.y)*width] - zbuffer[int(p.x) + int(p.y)*width];
-		maxangle = std::max(maxangle, atanf(elevation / distance));
-	}
-	return maxangle;
-
 }
 
-	struct ZShader : public IShader {
-		mat<4, 3, float> varying_tri;
+void change_size(int w, int h) {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0, 0, w, h);
+	glOrtho(-1, 1, -1, 1, -1, 8);
+	glMatrixMode(GL_MODELVIEW);
+}
 
-		virtual Vec4f vertex(int iface, int nthvert) {
-			Vec4f gl_Vertex = Projection * ModelView*embed<4>(model->vert(iface, nthvert));
-			varying_tri.set_col(nthvert, gl_Vertex);
-			return gl_Vertex;
-		}
+#if USE_SHADERS
+void printInfoLog(GLuint obj) {
+	int log_size = 0;
+	int bytes_written = 0;
+	glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &log_size);
+	if (!log_size) return;
+	char *infoLog = new char[log_size];
+	glGetProgramInfoLog(obj, log_size, &bytes_written, infoLog);
+	std::cerr << infoLog << std::endl;
+	delete[] infoLog;
+}
 
-		virtual bool fragment(Vec3f gl_FragCoord, Vec3f bar, TGAColor &color) {
-			color = TGAColor(0, 0, 0);
-			return false;
-		}
-	};
-
-	struct DepthShader : public IShader {
-		mat<3, 3, float> varying_tri;
-
-		DepthShader() : varying_tri() {}
-
-		virtual Vec4f vertex(int iface, int nthvert) {
-			Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
-			gl_Vertex = Viewport * Projection*ModelView*gl_Vertex;          // transform it to screen coordinates
-			varying_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
-			return gl_Vertex;
-		}
-
-		virtual bool fragment(Vec3f bar, TGAColor &color) {
-			Vec3f p = varying_tri * bar;
-			color = TGAColor(255, 255, 255)*(p.z / depth);
-			return false;
-		}
-	};
-
-
-	int main(int argc, char** argv) {
-		if (2 > argc) {
-			std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
-			return 1;
-		}
-
-		float *zbuffer = new float[width*height];
-		for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max());
-		model = new Model(argv[1]);
-
-		TGAImage frame(width, height, TGAImage::RGB);
-		lookat(eye, center, up);
-		viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
-		projection(-1.f / (eye - center).norm());
-
-		ZShader zshader;
-		for (int i = 0; i < model->nfaces(); i++) {
-			for (int j = 0; j < 3; j++) {
-				zshader.vertex(i, j);
-			}
-			triangle(zshader.varying_tri, zshader, frame, zbuffer);
-		}
-
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				if (zbuffer[x + y * width] < -1e5) continue;
-				float total = 0;
-				for (float a = 0; a < M_PI * 2 - 1e-4; a += M_PI / 4) {
-					total += M_PI / 2 - max_elevation_angle(zbuffer, Vec2f(x, y), Vec2f(cos(a), sin(a)));
-				}
-				total /= (M_PI / 2) * 8;
-				total = pow(total, 100.f);
-				frame.set(x, y, TGAColor(total * 255, total * 255, total * 255));
-			}
-		}
-
-		frame.flip_vertically();
-		frame.write_tga_file("framebuffer.tga");
-		delete[] zbuffer;
-		delete model;
-		return 0;
+bool read_n_compile_shader(const char *filename, GLuint &hdlr, GLenum shaderType) {
+	std::ifstream is(filename, std::ios::in | std::ios::binary | std::ios::ate);
+	if (!is.is_open()) {
+		std::cerr << "Unable to open file " << filename << std::endl;
+		return false;
 	}
+	long size = is.tellg();
+	char *buffer = new char[size + 1];
+	is.seekg(0, std::ios::beg);
+	is.read(buffer, size);
+	is.close();
+	buffer[size] = 0;
+
+	hdlr = glCreateShader(shaderType);
+	glShaderSource(hdlr, 1, (const GLchar**)&buffer, NULL);
+	glCompileShader(hdlr);
+	std::cerr << "info log for " << filename << std::endl;
+	printInfoLog(hdlr);
+	delete[] buffer;
+	return true;
+}
+
+void setShaders(GLuint &prog_hdlr, const char *vsfile, const char *fsfile) {
+	GLuint vert_hdlr, frag_hdlr;
+	read_n_compile_shader(vsfile, vert_hdlr, GL_VERTEX_SHADER);
+	read_n_compile_shader(fsfile, frag_hdlr, GL_FRAGMENT_SHADER);
+
+	prog_hdlr = glCreateProgram();
+	glAttachShader(prog_hdlr, frag_hdlr);
+	glAttachShader(prog_hdlr, vert_hdlr);
+
+	glLinkProgram(prog_hdlr);
+	std::cerr << "info log for the linked program" << std::endl;
+	printInfoLog(prog_hdlr);
+}
+#endif
+
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	glutCreateWindow("GLSL tutorial");
+	glClearColor(0.0, 0.0, 1.0, 1.0);
+
+	glutDisplayFunc(render_scene);
+	glutReshapeFunc(change_size);
+	glutKeyboardFunc(process_keys);
+
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+
+#if USE_SHADERS
+	glewInit();
+	if (GLEW_ARB_vertex_shader && GLEW_ARB_fragment_shader && GL_EXT_geometry_shader4)
+		std::cout << "Ready for GLSL - vertex, fragment, and geometry units" << std::endl;
+	else {
+		std::cout << "No GLSL support" << std::endl;
+		exit(1);
+	}
+	setShaders(prog_hdlr, "shaders/vert_shader.glsl", "shaders/frag_shader.glsl");
+	glUseProgram(prog_hdlr);
+#endif
+	glutMainLoop();
+	return 0;
+}
