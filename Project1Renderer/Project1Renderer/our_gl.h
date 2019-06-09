2964d249d0379f7ab0dbc64a300ae7eb97b7d85f
Spirit-78 <48299218+Spirit-78@users.noreply.github.com>
2019-06-09 13:41:31 +0200

    fixed small oversight and added gourard struct

--- Project1Renderer/Project1Renderer/our_gl.h
+++ Project1Renderer/Project1Renderer/our_gl.h
@@ -12,7 +12,7 @@ void lookat(Vec3f eye, Vec3f center, Vec3f up);
 struct IShader
 {
 	virtual ~IShader(); // hehe destruction
-	virtual Vec3i vertex(int iface, int nthvert) = 0;
+	virtual Vec4f vertex(int iface, int nthvert) = 0;
 	virtual bool fragment(Vec3f bar, TGAColor &color) = 0;
 };
 void triangle(Vec4f *pts, IShader &shader, TGAImage &image, TGAImage &zbuffer);
